73fad7f32a977dadc027850589227dc4616f87f7
Jose Antonio Rojas <joseucro1@gmail.com>
2023-02-28 14:33:17 -0400

    app.php Maatwebsite Excel ExcelServiceProvider class

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Intervention\Image\ImageServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
 
 
