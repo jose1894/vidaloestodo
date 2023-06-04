445d79bfe74776e99be2e2be39073f6e777e32a7
Jose Antonio Rojas <joseucro1@gmail.com>
2023-06-04 17:08:19 -0400

    Webservice Add Products

--- app/Http/Controllers/API/WebServices.php
+++ app/Http/Controllers/API/WebServices.php
@@ -13,16 +13,18 @@ use Illuminate\Support\Facades\DB;
 
 class WebServices extends Controller
 {
-    public function curl_products()
+    public function curl_products($variable)
     {
+        $url = "https://us-west-2.aws.data.mongodb-api.com/app/webservice-paqno/endpoint/v2/data?arg1=" . urlencode($variable);
+
         $curl = curl_init();
 
         curl_setopt_array($curl, [
-            CURLOPT_URL => "https://us-west-2.aws.data.mongodb-api.com/app/webservice-paqno/endpoint/v1/data",
+            CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 60,
+            CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "GET",
             CURLOPT_POSTFIELDS => "",
@@ -36,7 +38,7 @@ class WebServices extends Controller
         curl_close($curl);
 
         if ($err) {
-            echo "cURL Error #:" . $err;
+            return "cURL Error #:" . $err;
         } else {
             return $response;
         }
@@ -99,7 +101,7 @@ class WebServices extends Controller
     {
         foreach ($padres_hijos as $padre => $hijos) {
             $category = new Category();
-            $category->name             = $padre;
+            $category->name             = ucwords(strtolower($padre));
             $category->slug             = Str::slug($padre, '-');
             $category->parent_id        = NULL;
             $category->icon             = "<i class='fas fa-heartbeat'></i>";
@@ -116,7 +118,7 @@ class WebServices extends Controller
 
             foreach ($hijos as $hijo) {
                 $category = new Category();
-                $category->name             = $hijo;
+                $category->name             = ucwords(strtolower($hijo));
                 $category->slug             = Str::slug($hijo, '-');
                 $category->parent_id        = $id;
                 $category->icon             = "<i class='fas fa-heartbeat'></i>";
@@ -135,43 +137,55 @@ class WebServices extends Controller
 
     public function products_stelar()
     {
-        $categories = Category::get();
-        $products = $this->curl_products();        
-        $datos = json_decode($products, true);
+        $categories = Category::whereNull('parent_id')->get();
 
+         foreach ($categories as $category) {
+         $products = $this->curl_products(strtoupper($category['name']));
+        //$products = $this->curl_products(strtoupper('Aseo Y Uso'));
+        $datos = json_decode($products, true);
         $categories = Category::all()->toArray();
         $productIva = ProductIva::all()->toArray();
 
-        foreach ($datos as $dato) {
-            $departamento = $dato['Departamento'];
-            $grupo = $dato['Grupo'];
+        if ($datos) {
+            foreach ($datos as $dato) {
+                $departamento = ucwords(strtolower($dato['Departamento']));
+                $grupo = ucwords(strtolower($dato['Grupo']));
 
-            $indexDepartment = array_search($departamento, array_column($categories, 'name'));
-            $indexGroup = array_search($grupo, array_column($categories, 'name'));
-            $indexIva = array_search($dato['Iva'], array_column($productIva, 'productIva'));
+                $indexDepartment = array_search($departamento, array_column($categories, 'name'));
+                $indexGroup = array_search($grupo, array_column($categories, 'name'));
+                $indexIva = array_search($dato['Iva'], array_column($productIva, 'productIva'));
 
-            $product = new Product();
-            $product->brand_id          = NULL;
-            $product->sku               = $dato['SKU'];
-            $product->name              = $dato['Nombre'];
-            $product->slug              = Str::slug($dato['Nombre'], '-');
-            $product->description       = $dato['Descripcion'];
-            $product->base_price        = $dato['Precio'];
-            $product->prime_price       = $dato['Precio'];
-            $product->iva               = $dato['Iva'];
-            $product->iva_id            = $indexIva;
-            $product->save();
+                $padre = $categories[$indexDepartment]['id'];
+                $hijo = $categories[$indexGroup]['id'];
 
-            $product->categories()->attach([$categories[$indexDepartment]['id'], $categories[$indexGroup]['id']]);
-            $product->tags()->attach([1]);
+                $product = new Product();
+                $product->brand_id          = NULL;
+                $product->sku               = $dato['SKU'];
+                $product->name              = ucwords(strtolower($dato['Nombre']));
+                $product->slug              = Str::slug($dato['Nombre'], '-');
+                $product->description       = $dato['Descripcion'];
+                $product->base_price        = $dato['Precio'];
+                $product->prime_price       = $dato['Precio'];
+                $product->iva               = $dato['Iva'];
+                $product->iva_id            = $indexIva;
+                $product->save();
+                
+                $product->categories()->attach([$categories[$indexDepartment]['id'], $categories[$indexGroup]['id']]);
+                $product->tags()->attach([1]);
+                
+                $productStock = new ProductStock();
+                $productStock->product_id = $product->id;
+                $productStock->sku = $dato['SKU'];
+                $productStock->quantity = $dato['Stock'];
+                $productStock->save();
 
-            $productStock = new ProductStock();
-            $productStock->product_id = $product->id;
-            $productStock->sku = $dato['SKU'];
-            $productStock->quantity = $dato['Stock'];
-            $productStock->save();
+               // dd("Padre: " . $padre . " Hijo: " . $hijo. " Producto: ".$product->id);
+               //usleep(100000);
+            }
         }
 
+        // usleep(100000);
+         }
         // return response()->json(['categories' => $categories, 'products' => $products]);
     }
 }
