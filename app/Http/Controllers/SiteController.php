bb1172a6df981eb725ace5bb415e8be77cb13038
Jose Guerra <jose1894@gmail.com>
2023-06-28 14:58:53 -0400

    Megasoft test

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -1882,55 +1882,55 @@ class SiteController extends Controller
         return route('products.category', ['id' => $request->id, 'slug' => slug($request->slug)]);
     }
     public function megasoft(Request $request){
-        $curl = curl_init();
+    //     $curl = curl_init();
 
-        curl_setopt_array($curl, [
-            CURLOPT_URL => "https://paytest.megasoft.com.ve/payment/action/paymentgatewayuniversal-prereg?cod_afiliacion=2606202301&factura=1265&monto=" . $request->total,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_POSTFIELDS => "",
-            CURLOPT_COOKIE => "JSESSIONID=22F8EC805F50F4B1FD0ADE2E26311490",
-            CURLOPT_HTTPHEADER => [
-                "Authorization: Basic d3BhZ29zOkNhcmFjYXMxLg=="
-            ],
-        ]);
+    //     curl_setopt_array($curl, [
+    //         CURLOPT_URL => "https://paytest.megasoft.com.ve/payment/action/paymentgatewayuniversal-prereg?cod_afiliacion=2606202301&factura=1265&monto=" . $request->total,
+    //         CURLOPT_RETURNTRANSFER => true,
+    //         CURLOPT_ENCODING => "",
+    //         CURLOPT_MAXREDIRS => 10,
+    //         CURLOPT_TIMEOUT => 30,
+    //         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    //         CURLOPT_CUSTOMREQUEST => "GET",
+    //         CURLOPT_POSTFIELDS => "",
+    //         CURLOPT_COOKIE => "JSESSIONID=22F8EC805F50F4B1FD0ADE2E26311490",
+    //         CURLOPT_HTTPHEADER => [
+    //             "Authorization: Basic d3BhZ29zOkNhcmFjYXMxLg=="
+    //         ],
+    //     ]);
 
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
+    //     $response = curl_exec($curl);
+    //     $err = curl_error($curl);
 
-        curl_close($curl);
+    //     curl_close($curl);
 
-        if ($err) {
-            echo "cURL Error #:" . $err;
-        } 
+    //     if ($err) {
+    //         echo "cURL Error #:" . $err;
+    //     } 
 
-        $curl = curl_init();
+    //     $curl = curl_init();
 
-        curl_setopt_array($curl, [
-            CURLOPT_URL => "https://paytest.megasoft.com.ve/payment/action/paymentgatewayuniversal-data?control=". $response,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_POSTFIELDS => "",
-            CURLOPT_COOKIE => "JSESSIONID=22F8EC805F50F4B1FD0ADE2E26311490; JSESSIONID=5ED2A773BE4007132637A43C5303E2ED",
-        ]);
+    //     curl_setopt_array($curl, [
+    //         CURLOPT_URL => "https://paytest.megasoft.com.ve/payment/action/paymentgatewayuniversal-data?control=". $response,
+    //         CURLOPT_RETURNTRANSFER => true,
+    //         CURLOPT_ENCODING => "",
+    //         CURLOPT_MAXREDIRS => 10,
+    //         CURLOPT_TIMEOUT => 30,
+    //         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    //         CURLOPT_CUSTOMREQUEST => "GET",
+    //         CURLOPT_POSTFIELDS => "",
+    //         CURLOPT_COOKIE => "JSESSIONID=22F8EC805F50F4B1FD0ADE2E26311490; JSESSIONID=5ED2A773BE4007132637A43C5303E2ED",
+    //     ]);
 
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
+    //     $response = curl_exec($curl);
+    //     $err = curl_error($curl);
 
-        curl_close($curl);
+    //     curl_close($curl);
 
-        if ($err) {
-        echo "cURL Error #:" . $err;
-        } else {
-        echo $response;
-        }
-    }
+    //     if ($err) {
+    //     echo "cURL Error #:" . $err;
+    //     } else {
+    //     echo $response;
+    //     }
+    // }
 }
